e9ba4a1d12007bc291b2a19c6af76130559a6785
Prakash07-er <prakash.u.er@gmail.com>
2021-07-29 11:24:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Home from './components/Home';
+import CreateMentor from './components/CreateMentor';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import CreateStudent from './components/CreateStudent';
+import AssignStudent from './components/AssignStudent';
+import AssignMentor from './components/AssignMentor';
+import MetorAllStudent from './components/metorAllStudent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact /> 
+        <Route path="/create-mentor" component={CreateMentor} exact /> 
+        <Route path="/create-student" component={CreateStudent} exact /> 
+        <Route path="/assign-student/:id" component={AssignMentor} exact /> 
+        <Route path="/assign-mentor/:id" component={AssignStudent} exact /> 
+        <Route path="/perticular-mentor" component={MetorAllStudent} exact /> 
+
+      </Switch>
+    </Router>
+
+    </>
   );
 }
 
 export default App;
+
